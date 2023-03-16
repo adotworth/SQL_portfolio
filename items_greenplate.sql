d44f102b6a6bb1889a04350065bdbc16b74bc576
Andrew Worthington <121584287+adotworth@users.noreply.github.com>
2023-03-15 17:57:05 -0700

    Add files via upload

---
+++ items_greenplate.sql
@@ -0,0 +1,27 @@
+﻿-- add 30 vegan menu items to the items table
+INSERT INTO items (item_id, item_name, category, price, description)
+VALUES 
+    (1, 'Avocado Toast', 'Breakfast', 9.99, 'Toasted bread topped with smashed avocado and tomato'),
+    (2, 'Vegan Pancakes', 'Breakfast', 8.99, 'Fluffy pancakes made with almond milk and topped with fresh berries'),
+    (3, 'Tofu Scramble', 'Breakfast', 10.99, 'Scrambled tofu with veggies and spices, served with toast'),
+    (4, 'Acai Bowl', 'Breakfast', 11.99, 'Acai berries blended with banana and topped with granola and fruit'),
+    (5, 'Green Smoothie', 'Beverages', 6.99, 'Kale, spinach, banana, and almond milk blended together'),
+    (6, 'Vegan Burrito', 'Lunch', 12.99, 'Flour tortilla stuffed with rice, beans, veggies, and vegan cheese'),
+    (7, 'Falafel Wrap', 'Lunch', 10.99, 'Chickpea fritters with veggies and tahini sauce, wrapped in a tortilla'),
+    (8, 'Vegan Burger', 'Lunch', 13.99, 'Beyond Meat patty with lettuce, tomato, and vegan cheese on a bun'),
+    (9, 'Tofu Bowl', 'Lunch', 11.99, 'Marinated tofu with veggies and rice, topped with sesame seeds'),
+    (10, 'Vegan Caesar Salad', 'Salads', 9.99, 'Romaine lettuce, vegan croutons, and vegan Caesar dressing'),
+    (11, 'Kale Salad', 'Salads', 8.99, 'Kale, quinoa, avocado, and cherry tomatoes, tossed in a citrus dressing'),
+    (12, 'Vegan Cobb Salad', 'Salads', 10.99, 'Mixed greens topped with avocado, cherry tomatoes, and vegan bacon bits'),
+    (13, 'Vegan Meatballs', 'Appetizers', 7.99, 'Plant-based meatballs served with marinara sauce'),
+    (14, 'Sweet Potato Fries', 'Appetizers', 6.99, 'Crispy sweet potato fries with vegan aioli'),
+    (15, 'Vegan Nachos', 'Appetizers', 9.99, 'Tortilla chips topped with vegan cheese, beans, and veggies'),
+    (16, 'Mushroom Soup', 'Soups', 4.99, 'Creamy mushroom soup with thyme and garlic'),
+    (17, 'Butternut Squash Soup', 'Soups', 5.99, 'Roasted butternut squash soup with coconut milk'),
+    (18, 'Lentil Soup', 'Soups', 4.99, 'Hearty lentil soup with veggies and spices'),
+    (19, 'Vegan Chili', 'Soups', 6.99, 'Spicy vegan chili with beans and veggies'),
+    (20, 'Vegan Lasagna', 'Entrees', 14.99, 'Layered pasta with vegan cheese, tomato sauce, and veggies'),
+    (21, 'Vegan Enchiladas', 'Entrees', 13.99, 'Corn tortillas filled with black beans and veggies, topped with vegan cheese'),
+    (22, 'Vegan Meatloaf', 'Entrees', 15.99, 'Plant-based meatloaf served with mashed potatoes and gravy'),
+    (23, 'Stuffed Acorn Squash', 'Entrees', 16.99, 'Roasted acorn squash halves filled with a flavorful quinoa and vegetable stuffing, topped with dried cranberries, pecans, and fresh parsley')
+    ;
